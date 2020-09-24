ac6ffd954987d3742c2c4fedad990e43a8dd0dd6
Depdx <71102008+Depdx@users.noreply.github.com>
2020-09-24 18:22:38 -0400

    reponse

--- exercice.py
+++ exercice.py
@@ -4,21 +4,55 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	resultat =0
+	for letter in text:
+		 if str(letter).isalnum():
+			 resultat +=1
+	return resultat
 
 def get_word_length_histogram(text):
-	return [0]
+	listMot = text.split()
+	nbLettreMot = [get_num_letters(x) for x in listMot]
+	resultat = [0]*(max(nbLettreMot)+1)
+	for nb in nbLettreMot:
+		resultat[nb] += 1
+	return resultat
+
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
-
-	return ""
+	allignement = len(str(len(histogram)-1))
+	resultat = [f"{i: >{allignement}} {ROW_CHAR*elem}" for i,elem in enumerate(histogram) if i!=0]
+	return str.join("\n",resultat)
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
+	hauteur = max(histogram)
+	largeur = len(histogram)
+	histogramHelper = histogram.copy()
+	resultat= []
+	while max(histogramHelper) !=0:
+		str = ""
+		for i,elem in enumerate(histogramHelper):
+			if i==0:
+				continue
+			else:
+				if elem >= 1:
+					str += BLOCK_CHAR
+					histogramHelper[i] -=1
+				else:
+					str += " "
+		str += "\n"
+		resultat.append(str)
 
-	return ""
+	resultat.reverse()
+	histo=""
+	for elem in resultat:
+		histo += elem
+
+	histo += LINE_CHAR * largeur
+	return histo
 
 
 if __name__ == "__main__":
